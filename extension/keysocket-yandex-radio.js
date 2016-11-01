af389dc7d831b3dae1bb2f3e5a0dec3307a11ab1
Yury Khomlyak <yury.khomlyak@gmail.com>
2016-11-01 22:19:13 +0300

    fix next button for Yandex Radio

--- extension/keysocket-yandex-radio.js
+++ extension/keysocket-yandex-radio.js
@@ -3,11 +3,7 @@ var nextTarget = '.slider__item_next';
 
 function onKeyPress(key) {
     if (key === NEXT) {
-        var cards = document.querySelectorAll('.slider__item')
-        var nextCard = cards[cards.length - 3];
-        
-        console.log(nextCard);
-        simulateClick(nextCard.querySelector(nextTarget));
+        simulateClick(document.querySelector(nextTarget));
     } else if (key === PLAY) {
         simulateClick(document.querySelector(playTarget));
     }
